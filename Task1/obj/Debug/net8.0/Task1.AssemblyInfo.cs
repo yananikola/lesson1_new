b095dd114db82f9baafe2f917ffc856818ae07f9
Yana Nikolaeva <Nook10@inbox.ru>
2024-01-23 18:56:54 +0300

    добавили переменные Task1

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ded09926c8aa7e3276631352acfa632764a0eb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef1426ed77008a437473db5a9bb3789f3d8062fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
